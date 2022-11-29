6c0029d3d940f14a8f5269ed850a05d0d50983dc
petro_thomas <peter88tom@gmail.com>
2022-11-29 09:55:42 +0200

    Create a component and demonstrate how to use jsx with javascript

---
+++ src/TestComponent.jsx
@@ -0,0 +1,14 @@
+function TestComponent(){
+
+	const name = "Solid";
+	const style = "background-color:black; color:red";
+	return (
+		<div style={style}>
+		Testing {name}
+		</div>
+	)
+		
+
+}
+
+export default TestComponent;
